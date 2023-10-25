6a1b3086eb2fd499ed70e10a15048fa9a998e6ca
Ciptagusti <css.mil12@gmail.com>
2023-10-25 19:24:16 +0700

    Connected to Firebase

--- app/src/main/java/com/zak/firebasepractice/MainActivity.kt
+++ app/src/main/java/com/zak/firebasepractice/MainActivity.kt
@@ -2,10 +2,26 @@ package com.zak.firebasepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.zak.firebasepractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val reference: DatabaseReference = database.reference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setAction()
+    }
+    fun setAction() {
+        binding.btnSendData.setOnClickListener {
+            reference.child("Users").setValue(binding.edData.text.toString())
+        }
+
     }
 }
